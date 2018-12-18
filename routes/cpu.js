4d0d30896c7aaa82e293d61b5ee53ff6271676a8
KOBAYASHI <small1hayashi1m@yahoo.co.jp>
2018-12-18 22:18:52 +0900

    CPU対戦、最低限完成。 「クリア表示、同じ文字が使われている表示」は未実装。 小文字と大文字の区別あり（大文字の単語消す？） 予測値の文字数が固定されていない。

--- routes/cpu.js
+++ routes/cpu.js
@@ -14,6 +14,8 @@ router.get('/', (req, res, next) => {
             req.session.answer = ansAry;
             console.log(req.session.answer);
             req.session.prediction = [];
+            req.session.hit = [];
+            req.session.blow = [];
             if(!err) {
                 var data = {
                     title: 'VS CPU',
@@ -25,16 +27,60 @@ router.get('/', (req, res, next) => {
     });
 });
 
+/* POST処理 */
 router.post('/post', (req, res, next) => {
-    console.log(req.session.answer);
+    var ansAry = req.session.answer;
+    console.log(ansAry);
     var pre = req.body['predict'];
     var preAry = pre.split('');
     console.log(preAry);
+    
+    for (var i = 0; i < ansAry.length; i++) {
+        for (var j = i + 1; j < ansAry.length; j++) {
+            if (preAry[i] == preAry[j]) {
+                console.log('The same word is used!');
+                return false;
+            } else {
+                console.log('No Problem');
+            }
+        }
+    }
+
     req.session.prediction.push(pre);
     console.log(req.session.prediction);
+
+    //hit, blow数確認
+    var hit = 0;
+    var blow = 0;
+    for (var i = 0; i < preAry.length; i++) {
+        if (ansAry[i] == preAry[i]){
+            hit += 1;
+        }
+    }
+    for (var i = 0; i < preAry.length; i++) {
+        for (var j = 0; j < preAry.length; j++) {
+            if (ansAry[i] == preAry[j]) {
+                blow += 1;
+            }
+        }
+    }
+    blow -= hit;
+
+    if (hit != preAry.length) {
+        req.session.hit.push(hit);
+        req.session.blow.push(blow);
+    } else {
+        //CLEAR処理
+    }
+
+    console.log(req.session.hit);
+    console.log(req.session.blow);
+
     var data = {
         title: 'TEST',
-        content: req.session.prediction
+        content: req.session.prediction,
+        content2: req.session.hit,
+        content3: req.session.blow
     }
     res.render('cpu', data);
 });
