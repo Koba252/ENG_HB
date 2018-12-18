5a84061be3c8964537ffd79a2de7a252314994d3
KOBAYASHI <small1hayashi1m@yahoo.co.jp>
2018-12-18 16:49:12 +0900

    CPU対戦ページ追加

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// var createError = require('http-errors');
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var cpuRouter = require('./routes/cpu');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/cpu', cpuRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
