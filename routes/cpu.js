a783ef5f17972ef47970eb8b0d069c4abb9378c9
KOBAYASHI <small1hayashi1m@yahoo.co.jp>
2018-12-18 17:51:40 +0900

    DBからランダムに1語選択可能

--- routes/cpu.js
+++ routes/cpu.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var sqlite3 = require('sqlite3');
+
+var db = new sqlite3.Database('engdic.sqlite3');
 
 /* GET CPU */
 router.get('/', function(req, res, next) {
-  res.render('cpu', {
-    title: 'VS CPU' 
+    db.serialize(() => {
+        db.all("SELECT * FROM items ORDER BY RANDOM() LIMIT 1", (err, rows) => {
+            if(!err) {
+                var data = {
+                    title: 'VS CPU',
+                    content: rows
+                };
+                res.render('cpu', data);
+            }
+        });
     });
 });
 
