9c6954d53a31616921e77c0e343a0cb41652c1bf
KOBAYASHI <small1hayashi1m@yahoo.co.jp>
2018-12-18 18:38:39 +0900

    DBからランダムに1つの英単語取得可能

--- routes/cpu.js
+++ routes/cpu.js
@@ -7,7 +7,17 @@ var db = new sqlite3.Database('engdic.sqlite3');
 /* GET CPU */
 router.get('/', function(req, res, next) {
     db.serialize(() => {
-        db.all("SELECT * FROM items ORDER BY RANDOM() LIMIT 1", (err, rows) => {
+        db.get("SELECT * FROM items ORDER BY RANDOM()", (err, rows) => {
+            console.log(rows);
+            var ans = rows.word;
+            console.log(ans);
+            console.log(ans.length);
+            if (ans.length == 4) {
+                console.log('GAME START!');
+            } else {
+                console.log('Please Reload!');
+            }
+
             if(!err) {
                 var data = {
                     title: 'VS CPU',
