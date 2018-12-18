ca8409641ecc318424cc478c4f23ba5847e7495d
KOBAYASHI <small1hayashi1m@yahoo.co.jp>
2018-12-18 20:20:04 +0900

    セッションに値保存・保存した値の取り出し可能

--- public/javascripts/vs1.js
+++
@@ -1,2 +0,0 @@
-var ans; 
-console.log(ans);
--- routes/cpu.js
+++ routes/cpu.js
@@ -5,19 +5,28 @@ var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('engdic.sqlite3');
 
 /* GET CPU */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
     db.serialize(() => {
         db.get("SELECT * FROM items ORDER BY RANDOM()", (err, rows) => {
             console.log(rows);
+            req.session.answer = rows.word;
             if(!err) {
                 var data = {
                     title: 'VS CPU',
-                    ans: rows.word
+                    content: 'null'
                 };
                 res.render('cpu', data);
             }
         });
     });
+});
+
+router.post('/', (req, res, next) => {
+    var data = {
+        title: 'TEST',
+        content: req.session.answer
+    }
+    res.render('cpu', data);
 });
 
 
